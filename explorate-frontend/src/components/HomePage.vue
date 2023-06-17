5dea1d52899a92993b26cd3229565380f1bd3aba
Mark Li <mkli627554@gmail.com>
2023-06-16 23:14:14 -0500

    finished home page

--- explorate-frontend/src/components/HomePage.vue
+++ explorate-frontend/src/components/HomePage.vue
@@ -11,11 +11,30 @@
             <img class="empty" src="../assets/-Insert_image_here-.svg.png"/>
         </div>
     </div>
-    <div class="cards">
+    <div>
         <h2>Make the world a better place together</h2>
         <p>Get the community and resources you need all in one place</p>
-
-
+        <div class="cards">
+            <div class="volunteercard">
+                Become a volunteer
+            </div>
+            <div class="ngocard">
+                Partner with us
+            </div>
+            <div class="expeditioncard">
+                Join an expedition
+            </div>
+        </div>
+    </div>
+    <div class="perspective">
+        <div class="text">
+            <h2>A New Perspective Before You <br> Volunteer</h2>
+            <p>For decades international volunteers have relied mostly on marketing project info from volunteering organizations. 
+                Here, you’ll learn about real experience from veteran volunteers and the dos and don’ts to determine which program is right for you! </p>
+        </div>
+        <div class="image">
+            <img class="empty" src="../assets/-Insert_image_here-.svg.png"/>
+        </div>
     </div>
     <FooterMod />
 </template>
@@ -90,5 +109,51 @@ export default {
     background-color: white;
     color: #e7513f;
     margin-left: 30px;
+}
+
+.cards {
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+    margin-bottom: 50px;
+}
+
+.volunteercard {
+    background-color: gray;
+    height: 600px;
+    width: 300px;
+}
+
+.ngocard {
+    background-color: gray;
+    height: 600px;
+    width: 300px;
+}
+
+.expeditioncard {
+    background-color: gray;
+    height: 600px;
+    width: 300px;
+}
+
+.perspective {
+    overflow: hidden;
+}
+
+.text {
+    float: left;
+    width: 50%;
+    text-align: left;
+}
+
+.text p, .text h2 {
+    padding-right: 400px;
+    margin-left: 90px;
+}
+
+.image {
+    float:right;
+    width: 50%;
+    background-color: green;
 }
 </style>
