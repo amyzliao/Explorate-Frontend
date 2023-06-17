7723cd17c78de72d21d8b06643c74c8c9af86d69
Mark Li <mkli627554@gmail.com>
2023-06-16 21:47:03 -0500

    Revert "header bar"
    This reverts commit 867085e77e351aa6406aa1453627700c8ad00385.

--- explorate-frontend/src/components/HeaderMod.vue
+++ explorate-frontend/src/components/HeaderMod.vue
@@ -1,12 +1,9 @@
 <template>
     <div class="nav">
-        <button class="signup">Sign Up</button>
-        <button class="signin">Sign In</button>
-        <router-link to="/database">Database (temp)</router-link>
-        <router-link to="/">Volunteer</router-link>
-        <router-link to="/about">NGO Partner</router-link>
-        <router-link to="/resources">About Us</router-link>
-        <router-link to="/">Home (temp)</router-link>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/resources">Resources</router-link>
+        <router-link to="/database">Database</router-link>
     </div>
 </template>
 
@@ -18,45 +15,15 @@ export default {
 
 <style>
 .nav {
-    background-color: white;
-    padding: 10px;
-    overflow: hidden;
+    background-color: orange;
+    padding: 20px;
 }
 .nav a {
-    float: right;
     text-decoration: none;
-    color: #212121;
-    padding: 10px;
-    margin-left: 5px;
+    color: white;
+    padding: 20px;
 }
 .nav a:hover {
     background-color: orangered;
-}
-
-.nav button {
-    float: right;
-    text-decoration: none;
-    padding-top: 10px;
-    padding-bottom: 10px;
-    border: 1px solid #212121;
-    border-radius: 10px;
-}
-
-.signin {
-    float: right;
-    text-decoration: none;
-    padding: 20px;
-    background-color: white;
-    color: #212121;
-    margin-left: 20px;
-}
-
-.signup {
-    float: right;
-    text-decoration: none;
-    padding: 20px;
-    background-color: #212121;
-    color: white;
-    margin-left: 8px;
 }
 </style>
