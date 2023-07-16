1c4a35ccd7b060af0ad1d50af74c1ac100d663a8
Mark Li <mkli627554@gmail.com>
2023-07-16 00:27:48 -0500

    integrate api with frontend for database

--- explorate-api/controllers/ngo_opps.go
+++ explorate-api/controllers/ngo_opps.go
@@ -11,42 +11,51 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-type getAll struct {
-	ID          int
-	Name        sql.NullString
-	Title       sql.NullString
-	Description sql.NullString
-	Location    sql.NullString
-	Contact     sql.NullString
-	Website     sql.NullString
+type New_opp struct {
+	ID                       int
+	Org_name                 string
+	Org_location             string
+	Mission_statement        string
+	Impact_space             string
+	Un_goals                 string
+	Projects                 string
+	Values                   string
+	Benefits                 string
+	Ethical_criteria         string
+	Ethical_criteria_details string
+	Volunteer_location       string
+	Volunteer_skills         string
+	Skill_projects           string
+	Language_requirements    string
+	Volunteer_qualities      string
+	Work_env                 string
+	Accomodations            string
+	Cost                     string
+	Website                  string
+	Contact                  string
+	Photos                   string
+	Refer_others             string
+	Additional_info          string
+	Finding_volunteers       string
+	Org_logo                 string
+	Program_duration         string
+	Project_timeframe        string
+}
+
+func NewInput(s string) sql.NullString {
+	if len(s) == 0 {
+		return sql.NullString{}
+	}
+
+	return sql.NullString{
+		String: s,
+		Valid:  true,
+	}
 }
 
 // GetAll
 func GetAll(conn *pgx.Conn, table string) gin.HandlerFunc {
 	fn := func(c *gin.Context) {
-		// rows, err := conn.Query(context.Background(),
-		// 	"SELECT org_id, org_name, org_location, impact_space, projects, contact, website FROM "+table)
-		// if err != nil {
-		// 	fmt.Println("error:", err)
-		// }
-
-		// var opps []getAll
-		// for rows.Next() {
-		// 	var opp getAll
-		// 	if err := rows.Scan(
-		// 		&opp.ID,
-		// 		&opp.Name,
-		// 		&opp.Title,
-		// 		&opp.Description,
-		// 		&opp.Location,
-		// 		&opp.Contact,
-		// 		&opp.Website,
-		// 	); err != nil {
-		// 		fmt.Println(err)
-		// 	}
-		// 	opps = append(opps, opp)
-		// }
-
 		rows, err := conn.Query(context.Background(),
 			"SELECT * FROM "+table)
 		if err != nil {
@@ -58,7 +67,7 @@ func GetAll(conn *pgx.Conn, table string) gin.HandlerFunc {
 
 			var opp models.Ngo_opp
 			if err := rows.Scan(
-				&opp.Org_ID,
+				&opp.ID,
 				&opp.Org_name,
 				&opp.Org_location,
 				&opp.Mission_statement,
@@ -102,23 +111,44 @@ func GetAll(conn *pgx.Conn, table string) gin.HandlerFunc {
 func Find(conn *pgx.Conn, table string) gin.HandlerFunc {
 	fn := func(c *gin.Context) {
 		search_id := c.Param("id")
-		var opp getAll
+		var opp models.Ngo_opp
 
 		err := conn.QueryRow(context.Background(),
-			"SELECT org_id, org_name, org_location, impact_space, projects, contact, website FROM "+table+" WHERE org_id = $1 LIMIT 1", search_id).Scan(
+			"SELECT * FROM "+table+" WHERE ID = $1 LIMIT 1", search_id).Scan(
 			&opp.ID,
-			&opp.Name,
-			&opp.Title,
-			&opp.Description,
-			&opp.Location,
-			&opp.Contact,
+			&opp.Org_name,
+			&opp.Org_location,
+			&opp.Mission_statement,
+			&opp.Impact_space,
+			&opp.Un_goals,
+			&opp.Projects,
+			&opp.Values,
+			&opp.Benefits,
+			&opp.Ethical_criteria,
+			&opp.Ethical_criteria_details,
+			&opp.Volunteer_location,
+			&opp.Volunteer_skills,
+			&opp.Skill_projects,
+			&opp.Language_requirements,
+			&opp.Volunteer_qualities,
+			&opp.Work_env,
+			&opp.Accomodations,
+			&opp.Cost,
 			&opp.Website,
+			&opp.Contact,
+			&opp.Photos,
+			&opp.Refer_others,
+			&opp.Additional_info,
+			&opp.Finding_volunteers,
+			&opp.Org_logo,
+			&opp.Program_duration,
+			&opp.Project_timeframe,
 		)
 
 		if err != nil {
 			fmt.Println(err)
 		}
-
+		fmt.Println(opp)
 		c.IndentedJSON(http.StatusOK, opp)
 	}
 
@@ -128,16 +158,102 @@ func Find(conn *pgx.Conn, table string) gin.HandlerFunc {
 // Insert
 func Insert(conn *pgx.Conn, table string) gin.HandlerFunc {
 	fn := func(c *gin.Context) {
-		var input models.Ngo_opp
-
+		var input New_opp
+		// fmt.Println("works")
 		if err := c.ShouldBindJSON(&input); err != nil {
 			c.JSON(http.StatusBadRequest, err)
 			return
 		}
-
+		// fmt.Println(input)
+		// opp := models.Ngo_opp{
+		// 	// Org_name:                 NewInput(input.Org_name),
+		// 	// Org_location:             NewInput(input.Org_location),
+		// 	// Mission_statement:        NewInput(input.Mission_statement),
+		// 	// Impact_space:             NewInput(input.Impact_space),
+		// 	// Un_goals:                 NewInput(input.Un_goals),
+		// 	// Projects:                 NewInput(input.Projects),
+		// 	// Values:                   NewInput(input.Values),
+		// 	// Benefits:                 NewInput(input.Benefits),
+		// 	// Ethical_criteria:         NewInput(input.Ethical_criteria),
+		// 	// Ethical_criteria_details: NewInput(input.Ethical_criteria_details),
+		// 	// Volunteer_location:       NewInput(input.Volunteer_location),
+		// 	// Volunteer_skills:         NewInput(input.Volunteer_skills),
+		// 	// Skill_projects:           NewInput(input.Skill_projects),
+		// 	// Language_requirements:    NewInput(input.Language_requirements),
+		// 	// Volunteer_qualities:      NewInput(input.Volunteer_qualities),
+		// 	// Work_env:                 NewInput(input.Work_env),
+		// 	// Accomodations:            NewInput(input.Accomodations),
+		// 	// Cost:                     NewInput(input.Cost),
+		// 	// Website:                  NewInput(input.Website),
+		// 	// Contact:                  NewInput(input.Contact),
+		// 	// Photos:                   NewInput(input.Photos),
+		// 	// Refer_others:             NewInput(input.Refer_others),
+		// 	// Additional_info:          NewInput(input.Additional_info),
+		// 	// Finding_volunteers:       NewInput(input.Finding_volunteers),
+		// 	// Org_logo:                 NewInput(input.Org_logo),
+		// 	// Program_duration:         NewInput(input.Program_duration),
+		// 	// Project_timeframe:        NewInput(input.Project_timeframe),
+		// 	ID:                       input.ID,
+		// 	Org_name:                 input.Org_name,
+		// 	Org_location:             input.Org_location,
+		// 	Mission_statement:        input.Mission_statement,
+		// 	Impact_space:             input.Impact_space,
+		// 	Un_goals:                 input.Un_goals,
+		// 	Projects:                 input.Projects,
+		// 	Values:                   input.Values,
+		// 	Benefits:                 input.Benefits,
+		// 	Ethical_criteria:         input.Ethical_criteria,
+		// 	Ethical_criteria_details: input.Ethical_criteria_details,
+		// 	Volunteer_location:       input.Volunteer_location,
+		// 	Volunteer_skills:         input.Volunteer_skills,
+		// 	Skill_projects:           input.Skill_projects,
+		// 	Language_requirements:    input.Language_requirements,
+		// 	Volunteer_qualities:      input.Volunteer_qualities,
+		// 	Work_env:                 input.Work_env,
+		// 	Accomodations:            input.Accomodations,
+		// 	Cost:                     input.Cost,
+		// 	Website:                  input.Website,
+		// 	Contact:                  input.Contact,
+		// 	Photos:                   input.Photos,
+		// 	Refer_others:             input.Refer_others,
+		// 	Additional_info:          input.Additional_info,
+		// 	Finding_volunteers:       input.Finding_volunteers,
+		// 	Org_logo:                 input.Org_logo,
+		// 	Program_duration:         input.Program_duration,
+		// 	Project_timeframe:        input.Project_timeframe,
+		// }
+		// fmt.Println(opp.ID)
 		conn.Exec(context.Background(),
 			"INSERT INTO "+table+" VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28)",
-			input.Org_ID,
+			// opp.ID,
+			// opp.Org_name,
+			// opp.Org_location,
+			// opp.Mission_statement,
+			// opp.Impact_space,
+			// opp.Un_goals,
+			// opp.Projects,
+			// opp.Values,
+			// opp.Benefits,
+			// opp.Ethical_criteria,
+			// opp.Ethical_criteria_details,
+			// opp.Volunteer_location,
+			// opp.Volunteer_skills,
+			// opp.Skill_projects,
+			// opp.Language_requirements,
+			// opp.Volunteer_qualities,
+			// opp.Work_env,
+			// opp.Accomodations,
+			// opp.Cost,
+			// opp.Website,
+			// opp.Contact,
+			// opp.Photos,
+			// opp.Refer_others,
+			// opp.Additional_info,
+			// opp.Finding_volunteers,
+			// opp.Org_logo,
+			// opp.Program_duration,
+			// opp.Project_timeframe,
+			input.ID,
 			input.Org_name,
 			input.Org_location,
 			input.Mission_statement,
@@ -175,9 +291,52 @@ func Delete(conn *pgx.Conn, table string) gin.HandlerFunc {
 	fn := func(c *gin.Context) {
 		id := c.Param("id")
 		conn.Exec(context.Background(),
-			"DELETE FROM "+table+" WHERE org_id = $1", id)
+			"DELETE FROM "+table+" WHERE ID = $1", id)
 	}
 	return gin.HandlerFunc(fn)
 }
 
-// // Update
+// Update
+func Update(conn *pgx.Conn, table string) gin.HandlerFunc {
+	fn := func(c *gin.Context) {
+		var input New_opp
+		id := c.Param("id")
+
+		if err := c.ShouldBindJSON(&input); err != nil {
+			c.JSON(http.StatusBadRequest, err)
+			return
+		}
+		fmt.Println(input)
+		conn.Exec(context.Background(),
+			"UPDATE "+table+" SET org_name = $1, org_location = $2, mission_statement = $3, impact_space = $4, un_goals = $5, projects = $6, values = $7, benefits = $8, ethical_criteria = $9, ethical_criteria_details = $10, volunteer_location = $11, volunteer_skills = $12, skill_projects = $13, language_requirements = $14, volunteer_qualities = $15, work_env = $16, accomodations = $17, cost = $18, website = $19, contact = $20, photos = $21, refer_others = $22, additional_info = $23, finding_volunteers = $24, org_logo = $25, program_duration = $26, project_timeframe = $27 WHERE ID = $28",
+			input.Org_name,
+			input.Org_location,
+			input.Mission_statement,
+			input.Impact_space,
+			input.Un_goals,
+			input.Projects,
+			input.Values,
+			input.Benefits,
+			input.Ethical_criteria,
+			input.Ethical_criteria_details,
+			input.Volunteer_location,
+			input.Volunteer_skills,
+			input.Skill_projects,
+			input.Language_requirements,
+			input.Volunteer_qualities,
+			input.Work_env,
+			input.Accomodations,
+			input.Cost,
+			input.Website,
+			input.Contact,
+			input.Photos,
+			input.Refer_others,
+			input.Additional_info,
+			input.Finding_volunteers,
+			input.Org_logo,
+			input.Program_duration,
+			input.Project_timeframe,
+			id)
+	}
+	return gin.HandlerFunc(fn)
+}
--- explorate-frontend/src/components/database/DatabaseMod.vue
+++ explorate-frontend/src/components/database/DatabaseMod.vue
@@ -1,38 +1,3 @@
-<script>
-import { dbUseLoad, dbDelete } from '@/firebase'
-import DatabaseCard from './DatabaseCard.vue'
-
-export default {
-    name: 'DatabaseMod',
-    components: {
-        DatabaseCard
-    },
-    data() {
-        return {
-            oppCollection: []
-        }
-    },
-    methods: {
-        loadData() {
-            let result = dbUseLoad('opportunities')
-            this.oppCollection = result
-            console.log('load data')
-            console.warn(this.oppCollection.ownkeys)
-        },
-        deleteThisOpp(id) {
-            let result = dbDelete('opportunities', id)
-            console.warn(result)
-            if (result == 200) {
-                this.loadData()
-            }
-        }
-    },
-    async mounted() {
-        this.loadData()
-    }
-}
-</script>
-
 <template>
     <div class="mx-10 mt-6">
         <div class="flex flex-row gap-5">
@@ -48,8 +13,8 @@ export default {
             </div>
             <!-- column for displaying cards -->
             <div class="basis-3/4 bg-gray-50 grid grid-cols-3 gap-5">
-                <div v-for="opp in oppCollection" :key="opp.id">
-                    <DatabaseCard :name="opp.name" :location="opp.location" :mission="opp.description" :projects="opp.contact" :link="opp.website" />
+                <div v-for="opp in oppCollection" :key="opp.ID">
+                    <DatabaseCard :name="opp.Org_name.String" :location="opp.Org_location.String" :mission="opp.Impact_space.String" :projects="opp.Projects.String" :link="opp.Website.String" />
                 </div>
             </div>
         </div>
@@ -64,45 +29,32 @@ export default {
             <td>Description</td>
             <td>Location</td>
             <td>Website</td>
-            <td v-if="loggedIn()">Functions</td>
+            <td>Functions</td>
         </tr>
-<<<<<<< HEAD:explorate-frontend/src/components/DatabaseMod.vue
-        <tr v-for="opp in oppCollection" :key="opp.Org_ID">
+        <tr v-for="opp in oppCollection" :key="opp.ID">
             <td>{{ opp.Org_name.String }}</td>
             <td>{{ opp.Contact.String }}</td>
             <td>{{ opp.Impact_space.String }}</td>
             <td>{{ opp.Mission_statement.String }}</td>
             <td>{{ opp.Org_location.String }}</td>
             <td>{{ opp.Website.String }}</td>
-            <td v-if="loggedIn()">
-                <button v-on:click="goToEdit(opp.Org_ID)">Edit</button>
-                <br/>
-=======
-        <tr v-for="opp in oppCollection" :key="opp.id">
-            <td>{{ opp.name }}</td>
-            <td>{{ opp.contact }}</td>
-            <td>{{ opp.title }}</td>
-            <td>{{ opp.description }}</td>
-            <td>{{ opp.location }}</td>
-            <td>{{ opp.website }}</td>
             <td>
-                <router-link :to="'/database/edit/' + opp.id">Edit</router-link>
->>>>>>> 21d6eb17ad94e01ea88264cc27a97ddec23f71d4:explorate-frontend/src/components/database/DatabaseMod.vue
-                <button v-on:click="deleteThisOpp(opp.id)">Delete</button>
+                <router-link :to="'/database/edit/' + opp.ID">Edit</router-link>
+                <button v-on:click="deleteThisOpp(opp.ID)">Delete</button>
             </td>
-            <!-- <h1>oppcollection</h1>
-                <p>{{ oppCollection }}</p> -->
         </tr>
     </table>
 </template>
 
-<<<<<<< HEAD:explorate-frontend/src/components/DatabaseMod.vue
 <script>
-import { dbDelete } from '@/firebase';
-import axios from 'axios';
+import DatabaseCard from './DatabaseCard.vue'
+import axios from 'axios'
 
 export default {
     name: 'DatabaseMod',
+    components: {
+        DatabaseCard
+    },
     data() {
         return {
             oppCollection: []
@@ -110,35 +62,25 @@ export default {
     },
     methods: {
         async loadData() {
-            // let result = dbUseLoad('opportunities')
             let result = await axios.get("http://localhost:3000/ngo_opps")
             this.oppCollection = result.data
-            console.warn(this.oppCollection)
+            console.log(this.oppCollection)
         },
         deleteThisOpp(id) {
-            let result = dbDelete('opportunities', id)
+            let result = axios.delete("http://localhost:3000/ngo_opps/"+id)
             console.warn(result)
             if (result == 200) {
                 this.loadData()
+                location.reload()
             }
-        },
-        goToEdit(id) {
-            const path = "edit/" + id
-            this.$router.push(path)
-        },
-        loggedIn() {
-            const user = localStorage.getItem("user-info")
-            return user != null
+
         }
     },
-    async mounted() {
+    mounted() {
         this.loadData()
     }
 }
 </script>
-=======
-
->>>>>>> 21d6eb17ad94e01ea88264cc27a97ddec23f71d4:explorate-frontend/src/components/database/DatabaseMod.vue
 
 <style>
 table {
